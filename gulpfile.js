8d2548168b140db4b63200a44ec96f1e443615ca
ungsinatra <mamirov.21@mail.ru>
2022-03-21 20:34:37 +0300

    gulp 3.0

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ import { images } from './gulp/tasks/images.js'
 import { otfToTtf, ttfToWoff, fontStyle } from './gulp/tasks/fonts.js'
 import { svgSprive } from './gulp/tasks/svgSprive.js'
 import { zip } from './gulp/tasks/zip.js'
-import { ftp } from './gulp/tasks/ftp.js'
+// import { ftp } from './gulp/tasks/ftp.js'
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
@@ -43,13 +43,13 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
 const build = gulp.series(reset, mainTasks)
 const deployZIP = gulp.series(reset, mainTasks, zip)
-const deployFTP = gulp.series(reset, mainTasks, ftp)
+// const deployFTP = gulp.series(reset, mainTasks, ftp)
 
 // Экспорт сценариев
 export { dev }
 export { build }
 export { deployZIP }
-export { deployFTP }
+// export { deployFTP }
 
 // Выполнение сценария по умолчанию
 gulp.task('default', dev)
